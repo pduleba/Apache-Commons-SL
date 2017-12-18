d4830e37489e27c7307b65cf99365d864c85e623
pduleba <pduleba@pgs-soft.com>
2017-12-18 10:07:46 +0100

    Basic usage

--- apache-commons-sl/src/main/java/com/pduleba/apache/commons/sl/rng/RNG.java
+++ apache-commons-sl/src/main/java/com/pduleba/apache/commons/sl/rng/RNG.java
@@ -1,9 +1,18 @@
 package com.pduleba.apache.commons.sl.rng;
 
+import org.apache.commons.rng.UniformRandomProvider;
+import org.apache.commons.rng.simple.RandomSource;
+
 public class RNG {
 
 	public static void main(String[] args) {
+		// Instantiate a "Mersenne-Twister" generator with a factory method.
+		UniformRandomProvider rng = RandomSource.create(RandomSource.MT);
 
+		// Use it to produce a floating-point value between 0 and 1.
+		double random = rng.nextDouble();
+
+		System.out.println(random);
 	}
 
 }
