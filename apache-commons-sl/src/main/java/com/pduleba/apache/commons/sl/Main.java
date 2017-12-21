c902ca618c35ab28a52cb54fffa56654b28ee9a5
pduleba <pduleba@pgs-soft.com>
2017-12-21 10:34:26 +0100

    Apache Commons Lang3 :: Usage example

--- apache-commons-sl/src/main/java/com/pduleba/apache/commons/sl/Main.java
+++ apache-commons-sl/src/main/java/com/pduleba/apache/commons/sl/Main.java
@@ -1,9 +1,15 @@
 package com.pduleba.apache.commons.sl;
 
+import org.apache.commons.lang3.time.StopWatch;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		StopWatch started = StopWatch.createStarted();
+		Thread.sleep(500);
+		started.stop();
 
+		System.out.println("Time duration :: " + started.getTime());
 	}
 
 }
